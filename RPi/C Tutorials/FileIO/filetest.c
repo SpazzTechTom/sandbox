192b3de9c05e383a8eae998fa1d65866818a1dcd
Tom Olenik <projects@spazztech.com>
2018-11-29 15:16:04 +0000

    Backup from old Raspbian before upgrade

---
+++ RPi/C Tutorials/FileIO/filetest.c
@@ -0,0 +1,19 @@
+#include <time.h>
+#include <stdio.h>
+
+FILE *filePointer;
+
+int main()
+{
+	time_t timeStamp; 	/* create container to hold time value */
+	time(&timeStamp); 	/* fill timeStamp container by passing refernet to time() function */
+    printf("It is now %s", ctime(&timeStamp)); /* Print it */
+ 
+    filePointer = fopen(ctime(&timeStamp), "w");		/* Open file. Create it if it does not exist */
+    fprintf(filePointer, "\nThis is a test run at: "); 	/* Write to the file */
+    fprintf(filePointer, ctime(&timeStamp));			/* Write the time Stamp to the file */
+    
+    fclose(filePointer); 	/* Close the file */
+	return 0;
+}
+
